ec1f743a1e02c2f3d64820efe266fed054d93f11
luisf <luisfernandod.teixeira@gmail.com>
2025-04-16 16:18:57 -0300

    estilos

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,7 +7,19 @@ import { Component, Input } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
- @Input({ required: true, alias: 'text' }) buttonText: string = '';
- @Input({ required: true, alias: 'style' }) buttonStyle: 'White' | 'Purple' = 'White';
+ @Input({ required: true, alias: 'text' }) 
+ buttonText: string = '';
+ 
+ @Input({ required: true, alias: 'style' })
+  buttonStyle: 'white' | 'purple' = 'white';
+
+  @Input({ alias: 'disabled' })
+  isDisabled: boolean = false;
+
+  @Output('clicked') buttonClickedEmitt = new EventEmitter<void>();
+  
+  onButtonClicked(){
+    this.buttonClickedEmitt.emit();
+  }
 }
 
